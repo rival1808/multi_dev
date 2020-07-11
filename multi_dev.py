3a028b1eadb9ab0db36e93caeb6b580c633f2fcf
Iqbal_Dev <47203081+IqbalDev@users.noreply.github.com>
2020-07-12 03:27:36 +0700

    Update multi_dev.py

--- multi_dev.py
+++ multi_dev.py
@@ -47,42 +47,42 @@ def user_efbeh():
 
     for has in range(1, bag):
         # print nam + ' ' + str(has)
-        dev = open('data/data1', 'w')
+        dev = open('data/data1.txt', 'w')
         dev.write(dev+'\n')
         dev.close()
 
     # print ''
     for has in range(bag, bag1):
         # print nam + ' ' + str(has)
-        dev = open('data/data2', 'w')
+        dev = open('data/data2.txt', 'w')
         dev.write(dev+'\n')
         dev.close() 
 
     # print ''
     for has in range(bag1, bag2):
         # print nam + ' ' + str(has)
-        dev = open('data/data2', 'w')
+        dev = open('data/data2.txt', 'w')
         dev.write(dev+'\n')
         dev.close()
 
     # print ''
     for has in range(bag2, bag3):
         # print nam + ' ' + str(has)
-        dev = open('data/data4', 'w')
+        dev = open('data/data4.txt', 'w')
         dev.write(dev+'\n')
         dev.close()
 
     # print ''
     for has in range(bag3, bag4):
         # print nam + ' ' + str(has)
-        dev = open('data/data5', 'w')
+        dev = open('data/data5.txt', 'w')
         dev.write(dev+'\n')
         dev.close() 
 
     # print ''
     for has in range(bag4, jum+1):
         # print nam + ' ' + str(has)
-        dev = open('data/data6', 'w')
+        dev = open('data/data6.txt', 'w')
         dev.write(dev+'\n')
         dev.close() 
   except:
